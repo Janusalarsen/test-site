99582f32c2050d998deb3c26743bf7a765918cb0
Janus Larsen <janusalarsen@gmail.com>
2017-10-27 23:05:35 +0200

    add image change, add welcome message, add change user button

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,32 @@ myHeading.textContent = 'Janus Larsen';
 document.querySelector('a').onclick = function() {
     alert('Open Artstation in a new tab?');
 }
+var myImage = document.querySelector('img');
 
-document.querySelector('img').onclick = function() {
-    alert('Cannot enlarge');
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/mini_janus_6_tiny.png') {
+      myImage.setAttribute ('src', 'images/mini_hanne_6_tiny.png');
+    } else {
+      myImage.setAttribute ('images/mini_janus_6_tiny.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome, ' + storedName + '!';
+}
+myButton.onclick = function() {
+  setUserName();
 }
