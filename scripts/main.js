a8daf8b7ac45384ad5744df16c1bee23c6795974
Janus Larsen <janusalarsen@gmail.com>
2017-10-27 22:37:20 +0200

    add javascript file and link to index.html

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Janus Larsen';
+
+document.querySelector('a').onclick = function() {
+    alert('Open Artstation in a new tab?');
+}
+
+document.querySelector('img').onclick = function() {
+    alert('Cannot enlarge');
+}
